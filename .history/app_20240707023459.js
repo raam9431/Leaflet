cfd7718b177332b559bb3367824cb78172a8a8bf
raam9431 <118127013+raam9431@users.noreply.github.com>
2024-07-07 03:01:45 +0530

    5th commit

---
+++ .history/app_20240707023459.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+app.get("/", function (req, res) {
+    res.send("hey");
+});
+
+app.listen(3000, function () { 
+    console.log('server is running on port 3000');
+})
